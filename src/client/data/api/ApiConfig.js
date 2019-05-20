4aa458a00fa336c683bbb712fa7e53ac8a6c6901
Sharvan Jain <sharjain@in.ibm.com>
2019-05-20 21:49:47 +0530

    adding back end route

--- src/client/data/api/ApiConfig.js
+++ src/client/data/api/ApiConfig.js
@@ -1,10 +1,11 @@
 export const getApiHost = () => {
-  console.log('process.env.dev :: ', process.env.dev)
+  console.log('process.env.LOCAL_ECOMMERCE_BACK_ENV_APP_URL :: ', process.env.LOCAL_ECOMMERCE_BACK_ENV_APP_URL)
   /* if (process.env.dev) {
     return 'localhost:3000'
   } else {
     console.log('Not hosted till the date')
     // process.exit(1)
   } */
-  return 'http://localhost:3000'
+  // return 'http://localhost:3000'
+    return 'https://local-ecommerce-back-end-app.herokuapp.com'
 }
